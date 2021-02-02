34d89e77983299d1aa9905cc9ed5b35d20e8cadb
Mahdhaoui Mohamed Ali <46427608+MedAliMhdh@users.noreply.github.com>
2021-02-02 13:54:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const config = require('config');
 const connectDB = require('./config/db');
 
-
 PORT = config.get('PORT');
 connectDB();
 
@@ -12,15 +11,12 @@ app.use(express.json({ extended: false }));
 
 //Routes middleware
 app.use('/api/user', require('./routes/api/user'));
+app.use("/api/person", require("./routes/api/person"));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to home page');
-
+  res.send('API is running');
 });
 
-//Routes middlewware
-app.use("/api/person", require("./routes/api/person"));
-app.use("/api/person", require("./routes/api/person"));
 
 app.listen(PORT, () => {
   console.log(`app running on port  ${PORT}`);
