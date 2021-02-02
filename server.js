7037ba30a6d62b4b008e7288202aa818a6bade61
MohamedAli Mahdhaoui <med_ali_mahdhaoui@yahoo.fr>
2021-02-02 14:07:10 +0100

    server.js

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const config = require('config');
-const connectDB = require('./config/db');
+const config = require("config");
+const connectDB = require("./config/db");
 
-PORT = config.get('PORT');
+PORT = config.get("PORT");
 connectDB();
 
 //Init middleware
 app.use(express.json({ extended: false }));
 
 //Routes middleware
-app.use('/api/user', require('./routes/api/user'));
+app.use("/api/user", require("./routes/api/user"));
 app.use("/api/person", require("./routes/api/person"));
 
-app.get('/', (req, res) => {
-  res.send('API is running');
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
-
 
 app.listen(PORT, () => {
   console.log(`app running on port  ${PORT}`);
