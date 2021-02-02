d5c2123709cc1369b11ece92ab9af2e79def294f
MohamedAli Mahdhaoui <med_ali_mahdhaoui@yahoo.fr>
2021-02-02 13:43:10 +0100

    working on persons route

--- config/db.js
+++ config/db.js
@@ -1,18 +1,16 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const { mainModule } = require("process");
 
 const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      db,
-      { useNewUrlParser: true },
-      { useUnifiedTopology: true },
-      { useCreateIndex: true },
-      { useFindAndModify: false }
-    );
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    });
     console.log("connected to DB");
   } catch (error) {
     console.log("failed to connect to DB");
--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const app = express();
 const config = require("config");
 const connectDB = require("./config/db");
 
-PORT = config.get("PORT");
+PORT = config.get("PORT") || 5000;
+
+// const personRoute = ;
 connectDB();
+
+//Body parser middleware
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to home page");
 });
+
+//Routes middlewware
+app.use("/api/person", require("./routes/api/person"));
 
 app.listen(PORT, () => {
   console.log(`app running on port  ${PORT}`);
