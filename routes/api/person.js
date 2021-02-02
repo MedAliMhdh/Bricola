910859ec02ee8e994f0f4f1ce5e36908b2b4a26d
MohamedAli Mahdhaoui <med_ali_mahdhaoui@yahoo.fr>
2021-02-02 20:45:19 +0100

    Get Current user profile all/current user profile

--- routes/api/person.js
+++ routes/api/person.js
@@ -5,8 +5,24 @@ const User = require("../../models/User");
 const auth = require("../../middlewares/auth");
 // const { body, validationResult } = require("express-validator");
 const config = require("config");
-const { body } = require("express-validator");
-const { profile } = require("console");
+
+// @route    GET  api/profile/me
+// @desc     Get current person's profile
+// @access   Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const myProfile = await Person.findOne({
+      user: req.user.id,
+    }).populate("user", ["avatar", "name"]);
+    if (!myProfile) {
+      res.status(400).json({ message: "There is no profile for this user" });
+    }
+    res.json(myProfile);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
 
 //@route    POST api/person
 //@desc     Create/update person profile
@@ -78,6 +94,19 @@ router.delete("/", auth, async (req, res) => {
     await User.findOneAndRemove({ _id: req.user.id });
 
     res.json({ message: "User Deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+//@route    GET api/person/
+//@desc     Get all users
+//@access   Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const persons = await Person.find().populate("user", ["name", "avatar"]);
+    res.json(persons);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Server Error" });
