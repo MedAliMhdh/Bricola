d2193eeed797bdd36c48e8994bc65dc128e30484
Olfa <olfaghazel95@gmail.com>
2021-02-04 16:21:27 +0100

    just to pull

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -3,145 +3,142 @@ import { Link } from 'react-router-dom';
 const Register = () => {
   const [role, setRole] = useState('Person');
   return (
-    <div>
-      <div className='container'>
-        <br />
+    <div className='container'>
+      <br />
 
-        <div className='card bg-light'>
-          <article className='card-body mx-auto' style={{ maxWidth: '400' }}>
-            <h4 className='card-title mt-3 text-center'>Create Account</h4>
+      <div className='card bg-light'>
+        <article className='card-body mx-auto' style={{ maxWidth: '400' }}>
+          <h4 className='card-title mt-3 text-center'>Create Account</h4>
 
-            <form>
-              <div className='form-group input-group'>
-                <div className='input-group-prepend'>
-                  <span className='input-group-text'>
-                    {' '}
-                    <i className='fa fa-user'></i>{' '}
-                  </span>
-                </div>
-                <input
-                  name=''
-                  className='form-control'
-                  placeholder='Full name *'
-                  type='text'
-                />
+          <form>
+            <div className='form-group input-group'>
+              <div className='input-group-prepend'>
+                <span className='input-group-text'>
+                  {' '}
+                  <i className='fa fa-user'></i>{' '}
+                </span>
               </div>
-              {/* <!-- form-group// --> */}
-              <div className='form-group input-group'>
-                <div className='input-group-prepend'>
-                  <span className='input-group-text'>
-                    {' '}
-                    <i className='fa fa-envelope'></i>{' '}
-                  </span>
-                </div>
-                <input
-                  name=''
-                  className='form-control'
-                  placeholder='Email address *'
-                  type='email'
-                />
+              <input
+                name=''
+                className='form-control'
+                placeholder='Full name *'
+                type='text'
+              />
+            </div>
+            {/* <!-- form-group// --> */}
+            <div className='form-group input-group'>
+              <div className='input-group-prepend'>
+                <span className='input-group-text'>
+                  {' '}
+                  <i className='fa fa-envelope'></i>{' '}
+                </span>
               </div>
-              {/* <!-- form-group// --> */}
-              <div className='form-group input-group'>
-                <div className='input-group-prepend'>
-                  <span className='input-group-text'>
-                    {' '}
-                    <i className='fa fa-phone'></i>{' '}
-                  </span>
-                </div>
-                <input
-                  name=''
-                  className='form-control'
-                  placeholder='Phone number'
-                  type='text'
-                />
+              <input
+                name=''
+                className='form-control'
+                placeholder='Email address *'
+                type='email'
+              />
+            </div>
+            {/* <!-- form-group// --> */}
+            <div className='form-group input-group'>
+              <div className='input-group-prepend'>
+                <span className='input-group-text'>
+                  {' '}
+                  <i className='fa fa-phone'></i>{' '}
+                </span>
               </div>
-              {/* <!-- form-group// --> */}
+              <input
+                name=''
+                className='form-control'
+                placeholder='Phone number'
+                type='text'
+              />
+            </div>
+            {/* <!-- form-group// --> */}
+            <div className='form-group input-group'>
+              <div className='input-group-prepend'>
+                <span className='input-group-text'>
+                  {' '}
+                  <i className='fas fa-user-tag'></i>{' '}
+                </span>
+              </div>
+              <select
+                className='form-control'
+                onChange={(e) => {
+                  setRole(e.target.value);
+                }}
+              >
+                <option selected=''> Select your role</option>
+                <option value='Person'>Person</option>
+                <option value='Artisan'>Artisan</option>
+              </select>
+            </div>
+            {/* <!-- form-group// --> */}
+            {role === 'Artisan' && (
               <div className='form-group input-group'>
                 <div className='input-group-prepend'>
                   <span className='input-group-text'>
                     {' '}
-                    <i className='fas fa-user-tag'></i>{' '}
+                    <i className='fas fa-paint-roller'></i>
                   </span>
                 </div>
-                <select
-                  className='form-control'
-                  onChange={(e) => {
-                    setRole(e.target.value);
-                  }}
-                >
-                  <option selected=''> Select your role</option>
-                  <option value='Person'>Person</option>
-                  <option value='Artisan'>Artisan</option>
+                <select className='form-control'>
+                  <option selected=''> Select job type *</option>
+                  <option value='Bricklayer'>Bricklayer</option>
+                  <option value='Electrician'>Electrician</option>
+                  <option value='Painter'>Painter</option>
+                  <option value='Plumber'>Plumber</option>
+                  <option value='Mechanic'>Mechanic </option>
+                  <option value='Welder'>Welder</option>
+                  <option value='Carpenter'>Carpenter</option>
                 </select>
               </div>
-              {/* <!-- form-group// --> */}
-              {role === 'Artisan' && (
-                <div className='form-group input-group'>
-                  <div className='input-group-prepend'>
-                    <span className='input-group-text'>
-                      {' '}
-                      <i className='fas fa-paint-roller'></i>
-                    </span>
-                  </div>
-                  <select className='form-control'>
-                    <option selected=''> Select job type *</option>
-                    <option value='Bricklayer'>Bricklayer</option>
-                    <option value='Electrician'>Electrician</option>
-                    <option value='Painter'>Painter</option>
-                    <option value='Plumber'>Plumber</option>
-                    <option value='Mechanic'>Mechanic </option>
-                    <option value='Welder'>Welder</option>
-                    <option value='Carpenter'>Carpenter</option>
-                  </select>
-                </div>
-              )}
-              {/* <!-- form-group end.// --> */}
+            )}
+            {/* <!-- form-group end.// --> */}
 
-              <div className='form-group input-group'>
-                <div className='input-group-prepend'>
-                  <span className='input-group-text'>
-                    {' '}
-                    <i className='fa fa-lock'></i>{' '}
-                  </span>
-                </div>
-                <input
-                  className='form-control'
-                  placeholder='Create password *'
-                  type='password'
-                />
-              </div>
-              {/* <!-- form-group// --> */}
-              <div className='form-group input-group'>
-                <div className='input-group-prepend'>
-                  <span className='input-group-text'>
-                    {' '}
-                    <i className='fa fa-lock'></i>{' '}
-                  </span>
-                </div>
-                <input
-                  className='form-control'
-                  placeholder='Repeat password *'
-                  type='password'
-                />
-              </div>
-              {/* <!-- form-group// -->  */}
-              <div className='form-group'>
-                <button type='submit' className='btn btn-primary btn-block'>
+            <div className='form-group input-group'>
+              <div className='input-group-prepend'>
+                <span className='input-group-text'>
                   {' '}
-                  Create Account{' '}
-                </button>
+                  <i className='fa fa-lock'></i>{' '}
+                </span>
               </div>
-              {/* <!-- form-group// -->       */}
-              <p className='text-center'>
-                Have an account? <Link to='/login'>Log In</Link>{' '}
-              </p>
-            </form>
-          </article>
-        </div>
-        {/* <!-- card.// --> */}
+              <input
+                className='form-control'
+                placeholder='Create password *'
+                type='password'
+              />
+            </div>
+            {/* <!-- form-group// --> */}
+            <div className='form-group input-group'>
+              <div className='input-group-prepend'>
+                <span className='input-group-text'>
+                  {' '}
+                  <i className='fa fa-lock'></i>{' '}
+                </span>
+              </div>
+              <input
+                className='form-control'
+                placeholder='Repeat password *'
+                type='password'
+              />
+            </div>
+            {/* <!-- form-group// -->  */}
+            <div className='form-group'>
+              <button type='submit' className='btn btn-primary btn-block'>
+                {' '}
+                Create Account{' '}
+              </button>
+            </div>
+            {/* <!-- form-group// -->       */}
+            <p className='text-center'>
+              Have an account? <Link to='/login'>Log In</Link>{' '}
+            </p>
+          </form>
+        </article>
       </div>
-      {/* <!--container end.//--> */}
+      {/* <!-- card.// --> */}
     </div>
   );
 };
