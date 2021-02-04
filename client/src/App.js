260ca0ad7e222d08889176ea75b75f3453c7a6d6
MohamedAli Mahdhaoui <med_ali_mahdhaoui@yahoo.fr>
2021-02-04 15:10:35 +0100

    working on search box

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navb from "./components/Navb";
+import Search from "./components/Search";
 
 const App = () => {
   return (
     <div className='App'>
       <Navb />
+      <Search />
     </div>
   );
 };
