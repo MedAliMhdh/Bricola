6afc2899d199c9ad6fcdae01d07828ac1e132a7d
Olfa <olfaghazel95@gmail.com>
2021-02-06 22:50:33 +0100

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
-import "./App.css";
+import './App.css';
 
-import Register from "./components/Register";
-import Login from "./components/Login";
-import ArtisanCard from "./components/ArtisanCard";
-import { Switch, Route, Link } from "react-router-dom";
-import Home from "./components/Home";
-import ArtisanProfile from "./components/ArtisanProfile";
+import Register from './components/Register';
+import Login from './components/Login';
+import ArtisanCard from './components/ArtisanCard';
+import { Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import ArtisanProfile from './components/ArtisanProfile';
+// import Alert from './components/Alert';
+
+//Redux
+// import store from './store';
+// import { Provider } from 'react-redux';
 
 const App = () => {
   return (
+    // <Provider store={store}>
     <div className='App'>
       <Link to='/'></Link>
+      {/* <Alert /> */}
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/register' component={Register} />
@@ -19,6 +26,7 @@ const App = () => {
         <Route path='/artisanprofile' component={ArtisanProfile} />
       </Switch>
     </div>
+    // </Provider>
   );
 };
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
-import Navb from "./Navb";
+import React, { useState } from 'react';
+import Navb from './Navb';
 
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    console.log({ email, password });
+  };
+
   return (
     <div className=' loginContainer container-fluid mx-0 px-0'>
       <div className='container-fluid mx-0 px-0'></div>
@@ -12,22 +21,25 @@ const Login = () => {
         <br />
 
         <div className='card bg-light'>
-          <article className='card-body mx-auto' style={{ maxWidth: "400" }}>
+          <article className='card-body mx-auto' style={{ maxWidth: '400' }}>
             <h4 className='card-title mt-3 text-center'>Log In</h4>
 
-            <form className='fieldForm'>
+            <form onSubmit={(e) => onSubmit(e)}>
               <div className='form-group input-group'>
                 <div className='input-group-prepend'>
                   <span className='input-group-text'>
-                    {" "}
-                    <i className='fa fa-envelope'></i>{" "}
+                    {' '}
+                    <i className='fa fa-envelope'></i>{' '}
                   </span>
                 </div>
                 <input
-                  name=''
+                  name='email'
                   className='form-control'
                   placeholder='Email address'
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   type='email'
+                  required
                 />
               </div>
               {/* <!-- form-group// --> */}
@@ -37,27 +49,31 @@ const Login = () => {
               <div className='form-group input-group'>
                 <div className='input-group-prepend'>
                   <span className='input-group-text'>
-                    {" "}
-                    <i className='fa fa-lock'></i>{" "}
+                    {' '}
+                    <i className='fa fa-lock'></i>{' '}
                   </span>
                 </div>
                 <input
+                  name='password'
                   className='form-control'
                   placeholder='Create password'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   type='password'
+                  required
                 />
               </div>
               {/* <!-- form-group// --> */}
 
               <div className='form-group'>
                 <button type='submit' className='btn btn-primary btn-block'>
-                  {" "}
-                  Log In{" "}
+                  {' '}
+                  Log In{' '}
                 </button>
               </div>
               {/* <!-- form-group// -->       */}
               <p className='text-center'>
-                Not registred yet? <Link to='/register'>Register</Link>{" "}
+                Not registred yet? <Link to='/register'>Register</Link>{' '}
               </p>
             </form>
           </article>
