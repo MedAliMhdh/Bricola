521a3d44579da38ff20196720feefb79d66e7ee5
Olfa <olfaghazel95@gmail.com>
2021-02-09 00:34:09 +0100

    fixing errors

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -3,16 +3,12 @@ import {
   REGISTER_SUCCESS,
   USER_LOADED,
   AUTH_ERROR,
-<<<<<<< HEAD
   LOGIN_SUCCESS,
   LOGIN_FAIL,
 } from '../actions/types';
-=======
-} from "../actions/types";
->>>>>>> master
 
 const initialState = {
-  token: localStorage.getItem("token"),
+  token: localStorage.getItem('token'),
   isAuthenticated: null,
   loading: true,
   user: null,
@@ -22,7 +18,8 @@ const authReducer = (state = initialState, action) => {
   const { type, payload } = action;
   switch (type) {
     case REGISTER_SUCCESS:
-      localStorage.setItem("token", payload.token);
+    case LOGIN_SUCCESS:
+      localStorage.setItem('token', payload.token);
       return { ...state, ...payload, isAuthenticated: true, loading: false };
 
     case USER_LOADED:
@@ -33,7 +30,6 @@ const authReducer = (state = initialState, action) => {
         user: payload,
       };
 
-<<<<<<< HEAD
     case REGISTER_SUCCESS:
     case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
@@ -43,11 +39,11 @@ const authReducer = (state = initialState, action) => {
     case AUTH_ERROR:
     case LOGIN_FAIL:
       localStorage.removeItem('token');
-=======
+
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
       // localStorage.removeItem("token");
->>>>>>> master
       return {
         ...state,
         token: null,
