89914a1f3e43112fa02b755ba54a5126c149eda0
Olfa <olfaghazel95@gmail.com>
2021-02-08 12:22:51 +0100

    register

--- client/src/components/Alert.js
+++ client/src/components/Alert.js
@@ -12,7 +12,7 @@ const Alert = () => {
       </div>
     ));
   } else {
-    return 'pass';
+    return <></>;
   }
 };
 
--- client/src/components/ArtisanCard.js
+++ client/src/components/ArtisanCard.js
@@ -1,29 +1,29 @@
-import Thumbs from "./Thumbs";
+import Thumbs from './Thumbs';
 
 const ArtisanCard = ({ name, job, adress, bio, postsNumber }) => {
   return (
     <div className='container mt-5 d-flex justify-content-center'>
-      <div className='card p-4 mt-3'>
+      <div className='card artisanCard p-4 mt-3'>
         <div className='first'>
           <h6 className='heading'>{job}</h6>
         </div>
         <div className='second d-flex flex-row mt-2'>
           <div className='image mr-3'>
-            {" "}
+            {' '}
             <img
               src='https://i.imgur.com/0LKZQYM.jpg'
               className='rounded-circle'
               width='60'
               alt=''
-            />{" "}
+            />{' '}
           </div>
           <div className='row'>
             <div className='md-col-6 px-3'>
               <div className='d-flex flex-row mb-1'>
-                {" "}
+                {' '}
                 <span>{name}</span>
                 <div className='ratings ml-2'>
-                  {" "}
+                  {' '}
                   <Thumbs />
                 </div>
               </div>
@@ -31,13 +31,13 @@ const ArtisanCard = ({ name, job, adress, bio, postsNumber }) => {
                 <span>{adress}</span>
               </div>
               <div>
-                {" "}
+                {' '}
                 <button className='btn btn-outline-dark btn-sm'>
                   <i className='fas fa-phone'></i>
-                </button>{" "}
+                </button>{' '}
                 <button className='btn btn-outline-dark btn-sm'>
                   <i className='fas fa-comment'></i>
-                </button>{" "}
+                </button>{' '}
               </div>
             </div>
             <div className='md-col-6 px-3'>
@@ -48,7 +48,7 @@ const ArtisanCard = ({ name, job, adress, bio, postsNumber }) => {
         <hr className='line-color' />
         <h6>{postsNumber}</h6>
         <div className='third mt-4'>
-          {" "}
+          {' '}
           <button className='btn btn-success btn-block'>
             <i className='fa fa-clock-o'></i> See Profile
           </button>
