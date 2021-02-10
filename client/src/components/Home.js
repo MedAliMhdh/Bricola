83d4c81f5f643fd679c69f6874dc600edbc17552
Olfa <olfaghazel95@gmail.com>
2021-02-10 16:50:34 +0100

    rating component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Navb from './Navb';
 import Search from './Search';
 import CategoriesCarousel from './CategoriesCarousel';
 import Profiles from './Profiles';
 const Home = () => {
-  const [rate, setRate] = useState(0);
   return (
     <div className='container-fluid mx-0 px-0'>
       <Navb />
 
-      <Search setRate={setRate} rate={rate} />
+      <Search />
       <CategoriesCarousel />
 
       <Profiles />
