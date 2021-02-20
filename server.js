332e3c6c4ce9392af9e68f3818374893a838a912
MohamedAli Mahdhaoui <med_ali_mahdhaoui@yahoo.fr>
2021-02-20 23:26:25 +0100

    ..

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const config = require('config');
-const connectDB = require('./config/db');
-const cors = require('cors');
-const path = require('path');
+const config = require("config");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const path = require("path");
 
 PORT = process.env.PORT || 5000;
 
@@ -14,19 +14,19 @@ app.use(cors());
 app.use(express.json());
 
 //Routes middleware
-app.use('/api/user', require('./routes/api/user'));
-app.use('/api/person', require('./routes/api/person'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/post'));
-app.use('/api/artisan', require('./routes/api/artisan'));
+app.use("/api/user", require("./routes/api/user"));
+app.use("/api/person", require("./routes/api/person"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/post"));
+app.use("/api/artisan", require("./routes/api/artisan"));
 
 //Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   //Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'clien', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "clien", "build", "index.html"));
   });
 }
 
