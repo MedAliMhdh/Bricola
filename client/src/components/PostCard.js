c7dc15f6174063b9c7edec1149d75964cbd9f387
Olfa <olfaghazel95@gmail.com>
2021-02-22 19:12:09 +0100

    Cleaning post css file

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { updateLikes, deletePost, addComment } from "../actions/post";
-import { setAlert } from "../actions/alert";
-import { useDispatch, useSelector } from "react-redux";
-import "./CSS/artisanPost.css";
+import React, { useState, useEffect } from 'react';
+import { updateLikes, deletePost, addComment } from '../actions/post';
+import { setAlert } from '../actions/alert';
+import { useDispatch, useSelector } from 'react-redux';
+import './CSS/artisanPost.css';
 
-import CommentCard from "./CommentCard";
+import CommentCard from './CommentCard';
 
 const PostCard = ({
   photo,
@@ -19,8 +19,8 @@ const PostCard = ({
   const auth = useSelector((state) => state.auth);
 
   const defaultImg =
-    "http://www.gravatar.com/avatar/c1a276b8587995e9f29e1b7fe9148169?s=200&r=pg&d=mm";
-  const [text, setText] = useState("");
+    'http://www.gravatar.com/avatar/c1a276b8587995e9f29e1b7fe9148169?s=200&r=pg&d=mm';
+  const [text, setText] = useState('');
   const [showComments, setShowComments] = useState(false);
   const [showCommentBox, setShowCommentBox] = useState(false);
 
@@ -38,7 +38,7 @@ const PostCard = ({
       <div className='tab-content p-0'>
         {/* <!-- begin /profile-post tab --> */}
 
-        <div className='tab-pane fade active show' id='profile-post'>
+        <div className='tab-pane fade active show'>
           {/* <!-- begin timeline --> */}
           <div className='timeline-body'>
             <div className='timeline-header'>
@@ -51,7 +51,7 @@ const PostCard = ({
                 />
               </span>
               <span className='username'>{fullName}</span>
-              {((auth.user && auth.user.role === "Admin") ||
+              {((auth.user && auth.user.role === 'Admin') ||
                 (auth.user && auth.user._id === userId)) && (
                 <button
                   type='button'
@@ -67,19 +67,15 @@ const PostCard = ({
               <p>{content}</p>
               <img alt=''></img>
             </div>
-            <div className='timeline-likes'>
-              <div
-                className='stats-right'
-                onClick={() => setShowComments(!showComments)}
-              >
-                <span className='stats-text'>{commentsNumber} Comments</span>
-              </div>
-              <div className='stats'>
+            <div className='timeline-likes d-flex justify-content-between'>
+              <div>
                 <span className='fa-stack fa-fw stats-icon'>
-                  <i className='fa fa-circle fa-stack-2x text-primary'></i>
                   <i className='fa fa-thumbs-up fa-stack-1x fa-inverse'></i>
                 </span>
                 <span className='stats-total'>{likesNumber}</span>
+              </div>
+              <div onClick={() => setShowComments(!showComments)}>
+                <span className='stats-text'>{commentsNumber} Comments</span>
               </div>
             </div>
             <div className='timeline-footer'>
@@ -88,7 +84,7 @@ const PostCard = ({
                 onClick={() => {
                   auth.user
                     ? dispatch(updateLikes(id))
-                    : setAlert({ msg: "Please Connect", alertType: "danger" });
+                    : setAlert({ msg: 'Please Connect', alertType: 'danger' });
                 }}
               >
                 <i className='fa fa-thumbs-up fa-fw fa-lg m-r-3'></i>Like
@@ -97,7 +93,7 @@ const PostCard = ({
                 onClick={() => {
                   setShowCommentBox(auth.user ? true : false);
                   !auth.user &&
-                    setAlert({ msg: "Please Connect", alertType: "danger" });
+                    setAlert({ msg: 'Please Connect', alertType: 'danger' });
                 }}
               >
                 <i className='fa fa-comments fa-fw fa-lg m-r-3'></i>Comment
@@ -130,7 +126,7 @@ const PostCard = ({
                               dispatch(
                                 addComment({ postId: id, formData: text })
                               );
-                            setText("");
+                            setText('');
 
                             setShowComments(true);
                           }}
