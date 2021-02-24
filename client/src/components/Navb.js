a11eb4b0f42ec76a6eb77625a6a0c35f4c429430
Olfa <olfaghazel95@gmail.com>
2021-02-24 21:26:20 +0100

    Clearing profiles

--- client/src/components/Navb.js
+++ client/src/components/Navb.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Alert from "./Alert";
-import { useSelector, useDispatch } from "react-redux";
-import { logout } from "../actions/auth";
-import "./CSS/navbar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Alert from './Alert';
+import { clearProfiles } from '../actions/artisanProfile';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../actions/auth';
+import './CSS/navbar.css';
 
 const Navb = ({ setSearch, setRate, setJob, setEquipment, setCity }) => {
   const dispatch = useDispatch();
@@ -13,13 +14,13 @@ const Navb = ({ setSearch, setRate, setJob, setEquipment, setCity }) => {
   const resetStates = () => {
     setSearch(false);
     setRate(0);
-    setJob("");
-    setEquipment("");
-    setCity("");
+    setJob('');
+    setEquipment('');
+    setCity('');
   };
 
   const defaultImg =
-    "http://www.gravatar.com/avatar/c1a276b8587995e9f29e1b7fe9148169?s=200&r=pg&d=mm";
+    'http://www.gravatar.com/avatar/c1a276b8587995e9f29e1b7fe9148169?s=200&r=pg&d=mm';
 
   const guestLinks = (
     <div
@@ -53,7 +54,7 @@ const Navb = ({ setSearch, setRate, setJob, setEquipment, setCity }) => {
       <i class='far fa-bell fa-2x mx-2'></i>
       <i class='far fa-comment-alt fa-2x mx-2'></i>
       <form className='d-flex'>
-        {" "}
+        {' '}
         <div className='dropdown drpdwnMenuBtn ml-2'>
           <button
             className='btn dropdown-toggle px-0 py-0'
@@ -79,7 +80,7 @@ const Navb = ({ setSearch, setRate, setJob, setEquipment, setCity }) => {
             aria-labelledby='dropdownMenuButton'
           >
             {authState.user ? (
-              authState.user.role === "Artisan" ? (
+              authState.user.role === 'Artisan' ? (
                 <Link className='dropdown-item px-3' to='/artisanprofile/me'>
                   View profile
                 </Link>
@@ -117,6 +118,7 @@ const Navb = ({ setSearch, setRate, setJob, setEquipment, setCity }) => {
               className='navbar-brand mr-5'
               onClick={() => {
                 resetStates();
+                dispatch(clearProfiles());
               }}
             >
               Bricola
