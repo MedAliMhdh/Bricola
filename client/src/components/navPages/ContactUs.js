46daca09c4eb0ad8173842bcc9e1a67deed87f41
Olfa <olfaghazel95@gmail.com>
2021-02-26 00:56:39 +0100

    Contact us

--- client/src/components/navPages/ContactUs.js
+++ client/src/components/navPages/ContactUs.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React from 'react';
+import '../CSS/contact.css';
 
 const ContactUs = () => {
-  return <div></div>;
+  return (
+    <div className='d-flex justify-content-center'>
+      <section id='contact'>
+        <div className='section-content'>
+          <h1 className='section-header'>
+            Get in{' '}
+            <span
+              className='content-header wow fadeIn '
+              data-wow-delay='0.2s'
+              data-wow-duration='2s'
+            >
+              {' '}
+              Touch with us
+            </span>
+          </h1>
+        </div>
+        <div className='contact-section'>
+          <div className='container theContainer'>
+            <form>
+              <div className='form-group'>
+                <label for='exampleInputUsername'>Your name</label>
+                <input
+                  type='text'
+                  className='form-control'
+                  id=''
+                  placeholder=' Enter Your Name'
+                />
+              </div>
+              <div className='form-group'>
+                <label for='exampleInputEmail'>Email Address</label>
+                <input
+                  type='email'
+                  className='form-control'
+                  id='exampleInputEmail'
+                  placeholder=' Enter Your Email '
+                />
+              </div>
+              <div className='form-group'>
+                <label for='telephone'>Mobile Number</label>
+                <input
+                  type='tel'
+                  className='form-control'
+                  id='telephone'
+                  placeholder=' Enter Your Mobile Number.'
+                />
+              </div>
+
+              <div className='form-group'>
+                <label for='description'> Message</label>
+                <textarea
+                  className='form-control'
+                  id='description'
+                  placeholder='Enter Your Message'
+                ></textarea>
+              </div>
+              <div>
+                <button type='button' className='btn btn-default submit'>
+                  <i className='fa fa-paper-plane' aria-hidden='true'></i> Send
+                  Message
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default ContactUs;
--- client/src/components/navPages/Team.js
+++ client/src/components/navPages/Team.js
@@ -3,61 +3,61 @@ import '../CSS/team.css';
 
 const Team = () => {
   return (
-    <div class='py-5 team4'>
-      <div class='container'>
-        <div class='row justify-content-center mb-4'>
-          <div class='col-md-7 text-center'>
-            <h3 class='mb-3'>Experienced & Professional Team</h3>
-            <h6 class='subtitle'>
+    <div className='py-5 team4'>
+      <div className='container'>
+        <div className='row justify-content-center mb-4'>
+          <div className='col-md-7 text-center'>
+            <h3 className='mb-3'>Experienced & Professional Team</h3>
+            <h6 className='subtitle'>
               You can relay on our amazing features list and also our customer
               services will be great experience for you without doubt and in
               no-time
             </h6>
           </div>
         </div>
-        <div class='row d-flex justify-content-around'>
-          <div class='col-lg-3 mb-4'>
-            <div class='row'>
-              <div class='col-md-12'>
+        <div className='row d-flex justify-content-around'>
+          <div className='col-lg-3 mb-4'>
+            <div className='row'>
+              <div className='col-md-12'>
                 <img
                   src='mohamed.jpg'
                   alt='wrapkit'
-                  class='img-fluid rounded-circle'
+                  className='img-fluid rounded-circle'
                 />
               </div>
-              <div class='col-md-12 text-center'>
-                <div class='pt-2'>
-                  <h5 class='mt-4 font-weight-medium mb-0'>
+              <div className='col-md-12 text-center'>
+                <div className='pt-2'>
+                  <h5 className='mt-4 font-weight-medium mb-0'>
                     Mohamed Ali MAHDHAOUI
                   </h5>
-                  <h6 class='subtitle mb-3'>Full Stack Developer</h6>
+                  <h6 className='subtitle mb-3'>Full Stack Developer</h6>
                   <p>
                     You can relay on our amazing features list and also our
                     customer services will be great experience.
                   </p>
-                  <ul class='list-inline'>
-                    <li class='list-inline-item'>
+                  <ul className='list-inline'>
+                    <li className='list-inline-item'>
                       <a
                         href='https://www.linkedin.com/in/medalimhdh/'
-                        class='text-decoration-none d-block px-1'
+                        className='text-decoration-none d-block px-1'
                       >
-                        <i class='fab fa-linkedin-in'></i>
+                        <i className='fab fa-linkedin-in'></i>
                       </a>
                     </li>
-                    <li class='list-inline-item'>
+                    <li className='list-inline-item'>
                       <a
                         href='https://twitter.com/Mohamed79971643'
-                        class='text-decoration-none d-block px-1'
+                        className='text-decoration-none d-block px-1'
                       >
-                        <i class='fab fa-twitter'></i>
+                        <i className='fab fa-twitter'></i>
                       </a>
                     </li>
-                    <li class='list-inline-item'>
+                    <li className='list-inline-item'>
                       <a
                         href='mailto: med_ali_mahdhaoui@yahoo.fr'
-                        class='text-decoration-none d-block px-1'
+                        className='text-decoration-none d-block px-1'
                       >
-                        <i class='fas fa-at'></i>
+                        <i className='fas fa-at'></i>
                       </a>
                     </li>
                   </ul>
@@ -66,43 +66,43 @@ const Team = () => {
             </div>
           </div>
 
-          <div class='col-lg-3 mb-4'>
-            <div class='row'>
-              <div class='col-md-12'>
+          <div className='col-lg-3 mb-4'>
+            <div className='row'>
+              <div className='col-md-12'>
                 <img
                   src='/olfa.jpeg'
                   alt='wrapkit'
-                  class='img-fluid rounded-circle'
+                  className='img-fluid rounded-circle'
                 />
               </div>
-              <div class='col-md-12 text-center'>
-                <div class='pt-2'>
-                  <h5 class='mt-4 font-weight-medium mb-0'>Olfa GHAZEL</h5>
-                  <h6 class='subtitle mb-3'>Full Stack Developer</h6>
+              <div className='col-md-12 text-center'>
+                <div className='pt-2'>
+                  <h5 className='mt-4 font-weight-medium mb-0'>Olfa GHAZEL</h5>
+                  <h6 className='subtitle mb-3'>Full Stack Developer</h6>
                   <p>
                     You can relay on our amazing features list and also our
                     customer services will be great experience.
                   </p>
-                  <ul class='list-inline'>
-                    <li class='list-inline-item'>
+                  <ul className='list-inline'>
+                    <li className='list-inline-item'>
                       <a
                         href='https://www.linkedin.com/in/olfaghazel/'
-                        class='text-decoration-none d-block px-1'
+                        className='text-decoration-none d-block px-1'
                       >
-                        <i class='fab fa-linkedin-in'></i>
+                        <i className='fab fa-linkedin-in'></i>
                       </a>
                     </li>
-                    <li class='list-inline-item'>
-                      <a href='#' class='text-decoration-none d-block px-1'>
-                        <i class='fab fa-twitter'></i>
+                    <li className='list-inline-item'>
+                      <a href='#' className='text-decoration-none d-block px-1'>
+                        <i className='fab fa-twitter'></i>
                       </a>
                     </li>
-                    <li class='list-inline-item'>
+                    <li className='list-inline-item'>
                       <a
                         href='mailto: olfaghazel95@gmail.com'
-                        class='text-decoration-none d-block px-1'
+                        className='text-decoration-none d-block px-1'
                       >
-                        <i class='fas fa-at'></i>
+                        <i className='fas fa-at'></i>
                       </a>
                     </li>
                   </ul>
