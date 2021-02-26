be09a840639ecd0bffd76404e64473d0a144aa18
Olfa <olfaghazel95@gmail.com>
2021-02-26 14:18:39 +0100

    fixing Alert display

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import { useDispatch } from "react-redux";
-import { Switch, Route } from "react-router-dom";
-import Home from "./components/Home";
-import ArtisanProfile from "./components/ArtisanProfile";
-import PersonProfile from "./components/PersonProfile";
-import VisitedProfile from "./components/VisitedProfile";
-import About from "./components/navPages/About";
-import Blog from "./components/navPages/Blog";
-import ContactUs from "./components/navPages/ContactUs";
-import Team from "./components/navPages/Team";
-import Alert from "./components/Alert";
+import { useEffect, useState } from 'react';
+import './App.css';
+import Register from './components/Register';
+import Login from './components/Login';
+import { useDispatch, useSelector } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+import ArtisanProfile from './components/ArtisanProfile';
+import PersonProfile from './components/PersonProfile';
+import VisitedProfile from './components/VisitedProfile';
+import About from './components/navPages/About';
+import Blog from './components/navPages/Blog';
+import ContactUs from './components/navPages/ContactUs';
+import Team from './components/navPages/Team';
+import Alert from './components/Alert';
 
-import CreateProfile from "./components/profile-forms/CreateProfile";
-import EditProfile from "./components/profile-forms/EditProfile";
-import DeleteProfile from "./components/profile-forms/DeleteProfile";
-import { loadUser } from "./actions/auth";
-import setAuthToken from "./utils/setAuthToken";
-import Navb from "./components/Navb";
-import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import DeleteProfile from './components/profile-forms/DeleteProfile';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+import Navb from './components/Navb';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -28,11 +28,13 @@ if (localStorage.token) {
 const App = () => {
   const [search, setSearch] = useState(false);
   const [rate, setRate] = useState(0);
-  const [job, setJob] = useState("");
-  const [equipment, setEquipment] = useState("");
-  const [city, setCity] = useState("");
+  const [job, setJob] = useState('');
+  const [equipment, setEquipment] = useState('');
+  const [city, setCity] = useState('');
 
   const dispatch = useDispatch();
+
+  const alerts = useSelector((state) => state.alert);
 
   useEffect(() => {
     dispatch(loadUser());
@@ -47,12 +49,15 @@ const App = () => {
         setEquipment={setEquipment}
         setCity={setCity}
       />
-      <div className='alertsContainer'>
-        <Alert className='alerts' />
-      </div>
+      {alerts !== null && alerts.length > 0 && (
+        <div className='alertsContainer fixed-top'>
+          <Alert className='alerts' alerts={alerts} />
+        </div>
+      )}
+
       <Switch>
         <Route exact path='/'>
-          {" "}
+          {' '}
           <Home
             search={search}
             setSearch={setSearch}
